7d7ece9adf4fb9a7ca37e1d1d63b2e49674834c0
Juan José Mata <juanjo.mata@gmail.com>
2017-04-25 18:06:38 -0700

    Latest CDB and Crankshaft

--- 9.3-2.3/05-initdb-webodm.sh
+++ 9.3-2.3/05-initdb-webodm.sh
@@ -9,4 +9,5 @@ export PGUSER="$POSTGRES_USER"
 "${psql[@]}" <<- 'EOSQL'
 CREATE DATABASE webodm_dev;
 ALTER DATABASE webodm_dev SET postgis.gdal_enabled_drivers TO 'GTiff';
+ALTER DATABASE webodm_dev SET postgis.enable_outdb_rasters TO True;
 EOSQL
