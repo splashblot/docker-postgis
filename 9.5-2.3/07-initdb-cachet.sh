96df192f57bd85d840cdc5f45d58a97b0ed670f4
Juan José Mata <juanjo.mata@gmail.com>
2017-09-08 11:48:48 -0700

    Forgot one rename

--- 9.5-2.3/07-initdb-cachet.sh
+++ 9.5-2.3/07-initdb-cachet.sh
@@ -5,7 +5,7 @@ set -e
 # Perform all actions as $POSTGRES_USER
 export PGUSER="$POSTGRES_USER"
 
-# Create the Metabase DB
+# Create the Cachet DB
 "${psql[@]}" <<- 'EOSQL'
 CREATE DATABASE cachet;
 EOSQL
